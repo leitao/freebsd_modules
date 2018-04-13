3da92163d8c719d8c23424e0ec7387ab8fdb1c09
Breno Leitao <breno.leitao@gmail.com>
2018-04-13 15:48:18 +0100

    Improve dump

--- dscr/memcpy.c
+++ dscr/memcpy.c
@@ -8,15 +8,19 @@ int main(int argc, char *argv[]){
 	char *a, *b;
 	int i;
 	unsigned long dscr;
+        unsigned long foo;
 
 	if (argc < 2)
 		dscr = 0;
 	else
 		dscr = atol(argv[1]);
 
-	printf("dscr = %lx\n", dscr);
-
 	 __asm __volatile ("mtspr 0x11, %0" : "=r"(dscr));
+
+
+        asm ("mfdscr %0  \n\t" :  "=r" (foo) ::);
+        printf("BEGIN: dscr = %lx\n", foo);
+	foo = 0;
 
 	a = malloc(MAX);
 	b = malloc(MAX);
@@ -25,5 +29,7 @@ int main(int argc, char *argv[]){
 		memset(a, i, MAX);
 		memcpy(b, a, MAX);
 	}
-	printf("b now has %x\n", b[1]);
+
+        asm ("mfdscr %0  \n\t" :  "=r" (foo) ::);
+        printf("END: dscr = %lx\n", foo);
 }
